3218dbe43c2b3ae1f3b5e6b43c91729492123736
Téo Calvo <teocalvo2@gmail.com>
2020-12-17 22:03:53 -0300

    Implementaçao de GetAmount par saber saldo de pontos

--- points/points.go
+++ points/points.go
@@ -1,6 +1,7 @@
 package points
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -8,6 +9,15 @@ import (
 	"strconv"
 	"strings"
 )
+
+type Amount struct {
+	Channel       string `json:"channel"`
+	Username      string `json:"username"`
+	Points        int    `json:"points"`
+	PointsAlltime int    `json:"pointsAlltime"`
+	Watchtime     int    `json:"watchtime"`
+	Rank          int    `json:"rank"`
+}
 
 // Addpoints adiciona pontos para o usuário
 func Addpoints(username string, points int) {
@@ -29,6 +39,32 @@ func Addpoints(username string, points int) {
 	resp, _ := client.Do(req)
 	fmt.Println(resp)
 	resp.Body.Close()
+}
+
+//GetAmount obtem a informação de saldo de pontos do usuário
+func GetAmount(username string) int {
+
+	channelID := os.Getenv("CHANNEL_ID")
+	channelToken := os.Getenv("STREAMELEMENTS_TOKEN")
+
+	client := &http.Client{}
+	url := "https://api.streamelements.com/kappa/v2/points/%s/%s"
+	url = fmt.Sprintf(url, channelID, username)
+	req, _ := http.NewRequest(http.MethodGet, url, nil)
+
+	req.Header.Add("accept", "application/json")
+	req.Header.Add("content-type", "application/json")
+	req.Header.Add("authorization", "Bearer "+channelToken)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	amount := Amount{}
+	json.NewDecoder(resp.Body).Decode(&amount)
+	return amount.Points
 }
 
 // ReadNames lê lista de nomes
